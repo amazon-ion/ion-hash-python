03cb858c225d3070de31b4dd5d49eecdbbce83b1
Peter Cornell <pcornell@amazon.com>
2019-06-13 11:09:44 -0700

    conftest.py cleanup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,38 +1,27 @@
 # overrides pytest failure message for bytearray assertions
 def pytest_assertrepr_compare(op, left, right):
     if isinstance(left, bytearray) and isinstance(right, bytearray) and op == "==":
-        return ["bytearray mismatch:",
-                "expected: " + _hex_string(right),
-                "  actual: " + _hex_string(left)]
+        return [     "expected: " + _hex_string(right),
+                "       actual: " + _hex_string(left)]
 
     if isinstance(left, list) and isinstance(right, list) and op == "==":
-        result = ["bytearray mismatch:"]
-        result.append("expected:")
-        expected_strs = _hex_strings(right)
-        for s in expected_strs:
-            result.append("    " + s)
+        result = ["expected: ["]
+        for i in right:
+            result.append("      " + _hex_string(i) + ",")
+        result.append("     ]")
 
-        result.append("  actual:")
-        actual_strs = _hex_strings(left)
-        for s in actual_strs:
-            result.append("    " + s)
+        result.append("     actual: [")
+        for i in left:
+            result.append("      " + _hex_string(i) + ",")
+        result.append("     ]")
 
         return result
-
-
-def _hex_strings(lst):
-    strings = []
-    for item in lst:
-        strings.append(_hex_string(item))
-    return strings
 
 
 def _hex_string(_bytes):
     if _bytes is None:
         return 'None'
-    if isinstance(_bytes, bytearray):
-        return ''.join('{:02x} '.format(x) for x in _bytes)
-    if isinstance(_bytes, bytes):
-        return ' '.join('%02x' % x for x in _bytes)
+    if isinstance(_bytes, bytes) or isinstance(_bytes, bytearray):
+        return ''.join(' %02x' % x for x in _bytes)
     return _bytes
 
