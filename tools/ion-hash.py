e36a80ca6d5d9e657ce16c46c37108780b0b51b3
Peter Cornell <pcornell@amazon.com>
2020-01-03 15:01:32 -0800

    improvements to the cli tool

--- tools/ion-hash.py
+++ tools/ion-hash.py
@@ -11,41 +11,33 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-import fileinput
-from six import StringIO
 import sys
-
-from amazon.ion.core import IonEventType
-import amazon.ion.reader as ion_reader
-from amazon.ion.reader import NEXT_EVENT
-from amazon.ion.reader_binary import binary_reader
-from amazon.ion.reader_managed import managed_reader
-from amazon.ion.reader_text import text_reader
 import amazon.ion.simpleion as ion
-from ionhash.hasher import hash_reader
-from ionhash.hasher import HashEvent
-from ionhash.hasher import hashlib_hash_function_provider
+import ionhash
+
+
+if len(sys.argv) < 3:
+    print("Utility that prints the Ion Hash of the top-level values in a file.");
+    print();
+    print("Usage:");
+    print("  ion-hash [algorithm] [filename]");
+    print();
+    print("where [algorithm] is a hash function such as sha256");
+    print();
+    sys.exit(1);
 
 
 algorithm = sys.argv[1]
-input_file = []
-if len(sys.argv) > 2:
-    input_file = sys.argv[2]
+input_file = sys.argv[2]
 
-for line in fileinput.input(input_file):
+f = open(input_file)
+values = ion.loads(f.read(), single_value=False)
+f.close()
+
+for value in values:
     try:
-        hr = hash_reader(
-                ion_reader.blocking_reader(managed_reader(text_reader(is_unicode=True), None), StringIO(line)),
-                hashlib_hash_function_provider(algorithm))
-        while True:
-            event = hr.send(NEXT_EVENT)
-            if event.event_type == IonEventType.STREAM_END:
-                break
-
-        digest = hr.send(HashEvent.DIGEST)
-        print(''.join('%02x ' % x for x in digest)[0:-1])
-    except EOFError:
-        break
+        digest = value.ion_hash('md5')
+        print(''.join('%02x ' % x for x in value.ion_hash('md5'))[0:-1])
     except Exception as e:
         print('[unable to digest: ' + str(e) + ']')
 
